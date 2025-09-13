7f516181c6a5b0daa071c4fbe9888c94b90fc3db
blah <blah>
2025-09-13 16:42:15 -0700

    Code cleanup

--- spennyIRC.Scripting/IrcCommands.cs
+++ spennyIRC.Scripting/IrcCommands.cs
@@ -2,7 +2,6 @@
 
 namespace spennyIRC.Scripting;
 
-
 public class IrcCommands : IIrcCommands
 {
     public Dictionary<string, IIrcCommand> Commands { get; private set; } = new (StringComparer.OrdinalIgnoreCase);
@@ -17,16 +16,6 @@ public class IrcCommands : IIrcCommands
         return true;
     }
 
-    //public bool AddCommand(string name, IIrcCommand command)
-    //{
-    //    if (Commands.TryGetValue(name, out _))
-    //    {
-    //        return false;
-    //    }
-    //    Commands[name] = command;
-    //    return true;
-    //}
-
     public async Task ExecuteCommand(string name, string? parameters, IIrcSession session)
     {
         if (Commands.TryGetValue(name, out IIrcCommand? foundCommand))
@@ -35,3 +24,13 @@ public class IrcCommands : IIrcCommands
         }
     }
 }
+
+//public bool AddCommand(string name, IIrcCommand command)
+//{
+//    if (Commands.TryGetValue(name, out _))
+//    {
+//        return false;
+//    }
+//    Commands[name] = command;
+//    return true;
+//}
