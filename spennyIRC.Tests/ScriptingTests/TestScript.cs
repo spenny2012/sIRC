02db99fab37b316830d0ab05487090f52eebf3e9
spencerkdev@gmail.com <spencerkdev@gmail.com>
2025-11-08 21:45:56 -0800

    Added primary constructor to HelloWorldScript in TestScript.cs

--- spennyIRC.Tests/ScriptingTests/TestScript.cs
+++ spennyIRC.Tests/ScriptingTests/TestScript.cs
@@ -1,8 +1,9 @@
+using spennyIRC.Scripting.Commands;
 using spennyIRC.Scripting.Engine;
 using System;
 #pragma warning disable IDE0079 // Remove unnecessary suppression
 #pragma warning disable CA1050  // Remove declare types in namespace
-public class HelloWorldScript : SircScript
+public class HelloWorldScript(IIrcCommands commands) : SircScript(commands)
 {
     public override string Name => "Hello World Script"; // Required
     //public override string Version => "1.0";
