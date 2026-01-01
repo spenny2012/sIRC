d917b250b3155205b95bfca646fa42344c0d3161
spencerkdev@gmail.com <spencerkdev@gmail.com>
2026-01-01 11:08:59 -0800

    Reorganized

--- spennyIRC.Core/IRC/Helpers/IrcProtocolHelpers.cs
+++ spennyIRC.Core/IRC/Helpers/IrcProtocolHelpers.cs
@@ -4,6 +4,149 @@ namespace spennyIRC.Core.IRC.Helpers;
 
 public static class IrcProtocolHelpers
 {
+    public static (string nick, IrcChannelAccessType[] access) ExtractAccessFromNick(this string nick) // TODO: maybe redo this entirely.. this is for 353 and 353 only seems to show 1 channel access mode on IRCnet
+    {
+        Span<IrcChannelAccessType> accessSpan = stackalloc IrcChannelAccessType[5];
+        int accessCount = 0;
+        ReadOnlySpan<char> nickSpan = nick.AsSpan();
+
+        int maxLength = nickSpan.Length < 5 ? nickSpan.Length : 5;
+        for (int i = 0; i < maxLength; i++)
+        {
+            char currentChar = nickSpan[i];
+            IrcChannelAccessType accessType = currentChar.AccessSymbolToAccessType();
+            if (accessType != IrcChannelAccessType.NONE)
+            {
+                accessSpan[accessCount++] = accessType;
+            }
+        }
+
+        string resultNick = accessCount > 0 ? nickSpan[accessCount..].ToString() : nick;
+        IrcChannelAccessType[] resultAccess = accessSpan[..accessCount].ToArray();
+
+        return (resultNick, resultAccess);
+    }
+
+    public static (string nick, IrcChannelAccessType[] access) ExtractAccessFromNick(this ReadOnlySpan<char> nickSpan)
+    {
+        // Use stackalloc for temporary storage of access types
+        Span<IrcChannelAccessType> accessSpan = stackalloc IrcChannelAccessType[5];
+        int accessCount = 0;
+
+        // Iterate through the nick to extract access prefixes
+        int maxLength = nickSpan.Length < 5 ? nickSpan.Length : 5;
+        for (int i = 0; i < maxLength; i++)
+        {
+            char currentChar = nickSpan[i];
+            IrcChannelAccessType accessType = currentChar.AccessSymbolToAccessType();
+            if (accessType != IrcChannelAccessType.NONE)
+            {
+                accessSpan[accessCount++] = accessType;
+            }
+        }
+
+        // Extract the nick without access prefixes
+        string resultNick = accessCount > 0 ? nickSpan[accessCount..].ToString() : nickSpan.ToString();
+        IrcChannelAccessType[] resultAccess = accessSpan[..accessCount].ToArray();
+        return (resultNick, resultAccess);
+    }
+
+    /// <summary>
+    /// This function is to be used for numeric 352
+    /// </summary>
+    ///
+    public static IrcChannelAccessType[] ExtractAccessTypeFrom352(this string modes)
+    {
+        Span<IrcChannelAccessType> accessSpan = stackalloc IrcChannelAccessType[5]; // Max 5 access types
+        int accessCount = 0;
+        ReadOnlySpan<char> modesSpan = modes.AsSpan();
+
+        for (int i = 0; i < modesSpan.Length; i++)
+        {
+            char mode = modesSpan[i];
+            if (char.IsLetter(mode)) continue;
+
+            IrcChannelAccessType accessType = mode.AccessSymbolToAccessType();
+            if (accessType != IrcChannelAccessType.NONE && accessCount < 5)
+                accessSpan[accessCount++] = accessType;
+        }
+
+        return accessSpan[..accessCount].ToArray();
+    }
+
+    public static IrcExtractedUserInfo ExtractBasicUserInfo(this string fullNick)
+    {
+        ReadOnlySpan<char> fullNickSpan = fullNick.AsSpan();
+        int nickEnd = fullNickSpan.IndexOf('!');
+        if (nickEnd < 0)
+        {
+            throw new FormatException("Invalid format: '!' not found.");
+        }
+
+        int identEnd = fullNickSpan[(nickEnd + 1)..].IndexOf('@');
+        if (identEnd < 0)
+        {
+            throw new FormatException("Invalid format: '@' not found.");
+        }
+        identEnd += nickEnd + 1;
+
+        return new IrcExtractedUserInfo
+        {
+            Nick = fullNickSpan[..nickEnd].ToString(),
+            Ident = fullNickSpan[(nickEnd + 1)..identEnd].ToString(),
+            Domain = fullNickSpan[(identEnd + 1)..].ToString()
+        };
+    }
+
+    /// <summary>
+    /// Extracts channel user mode changes from IRC message parts.
+    /// </summary>
+    /// <param name="messageParts">The IRC message split into parts, where index 3 is the mode string and 4 onward are user names.</param>
+    /// <returns>A dictionary mapping users to their mode changes, with each change as a tuple of (isAdding, accessType).</returns>
+    /// <exception cref="ArgumentException">Thrown if messageParts has fewer than 4 elements.</exception>
+    public static Dictionary<string, List<(bool isAdding, IrcChannelAccessType type)>> ExtractChannelUserModes(this string[] messageParts)
+    {
+        string modeString = messageParts[3];
+        string[] affectedUsers = messageParts[4..];
+        Dictionary<string, List<(bool isAdding, IrcChannelAccessType type)>> userModeChanges = [];
+
+        bool isAdding = false;
+        int userIndex = 0;
+
+        for (int i = 0; i < modeString.Length; i++)
+        {
+            char currentChar = modeString[i];
+            if (currentChar == '+')
+            {
+                isAdding = true;
+            }
+            else if (currentChar == '-')
+            {
+                isAdding = false;
+            }
+            else if (char.IsLetter(currentChar) && userIndex < affectedUsers.Length)
+            {
+                string user = affectedUsers[userIndex];
+                IrcChannelAccessType accessType = currentChar.ModeLetterToAccessType();
+
+                if (accessType != IrcChannelAccessType.NONE)
+                {
+                    if (userModeChanges.TryGetValue(user, out List<(bool isAdding, IrcChannelAccessType type)>? changes))
+                    {
+                        changes.Add((isAdding, accessType));
+                    }
+                    else
+                    {
+                        userModeChanges[user] = [(isAdding, accessType)];
+                    }
+                }
+                userIndex++;
+            }
+        }
+
+        return userModeChanges;
+    }
+
     //public static string ExtractFullHost(this string line)
     //{
     //    return line[(line.IndexOf('!') + 1)..];
@@ -13,6 +156,103 @@ public static class IrcProtocolHelpers
         ReadOnlySpan<char> span = line.AsSpan();
         int index = span.IndexOf('!');
         return index == -1 ? [] : span[(index + 1)..];
+    }
+
+    public static Dictionary<string, string> ExtractNetworkSettingsFrom005(this string line)
+    {
+        Dictionary<string, string> dictionary = [];
+        Regex regex = new(@"(\S+)=([^\s]+)");
+
+        MatchCollection matches = regex.Matches(line);
+        foreach (Match match in matches)
+        {
+            string key = match.Groups[1].Value;
+            string value = match.Groups[2].Value;
+            dictionary[key] = value;
+        }
+        return dictionary;
+    }
+
+    public static string ExtractTrailing(this string line, int startIndex) // TODO: make this less weird
+    {
+        ReadOnlySpan<char> span = line.AsSpan();
+        int currentIndex = 0;
+        int partCount = 0;
+
+        while (partCount < startIndex)
+        {
+            int spaceIndex = span[currentIndex..].IndexOf(' ');
+            if (spaceIndex == -1)
+            {
+                return string.Empty;
+            }
+            currentIndex += spaceIndex + 1;
+            partCount++;
+        }
+
+        ReadOnlySpan<char> remaining = span[currentIndex..];
+        int colonIndex = remaining.IndexOf(':');
+        if (colonIndex == -1)
+        {
+            return remaining.ToString();
+        }
+
+        return remaining[(colonIndex + 1)..].ToString();
+    }
+
+    public static IrcExtractedUserInfo ExtractUserInfoFrom352(this string[] lineParts)
+    {
+        string nick = lineParts[7];
+        string ident = lineParts[4];
+        string domain = lineParts[5];
+        string userAccessModes = lineParts[8];
+        IrcChannelAccessType[] access = userAccessModes.ExtractAccessTypeFrom352();
+        return new IrcExtractedUserInfo
+        {
+            Nick = nick,
+            Ident = ident,
+            Domain = domain,
+            Access = access
+        };
+    }
+
+    public static IrcExtractedUserInfo ExtractUserInfoFrom353(this string fullHostString)
+    {
+        ReadOnlySpan<char> fullHostSpan = fullHostString.AsSpan();
+
+        int nickEnd = fullHostSpan.IndexOf('!');
+        if (nickEnd == -1)
+        {
+            (string nick, IrcChannelAccessType[] access) = ExtractAccessFromNick(fullHostSpan);
+            return new IrcExtractedUserInfo
+            {
+                Nick = nick.ToString(),
+                Access = access
+            };
+        }
+        else
+        {
+            int identEnd = fullHostSpan[(nickEnd + 1)..].IndexOf('@');
+            if (identEnd == -1)
+            {
+                throw new FormatException("Invalid format: '@' not found.");
+            }
+            identEnd += nickEnd + 1;
+
+            ReadOnlySpan<char> nickSpan = fullHostSpan[..nickEnd];
+            ReadOnlySpan<char> identSpan = fullHostSpan[(nickEnd + 1)..identEnd];
+            ReadOnlySpan<char> domainSpan = fullHostSpan[(identEnd + 1)..];
+
+            (string nick, IrcChannelAccessType[] access) = ExtractAccessFromNick(nickSpan);
+
+            return new IrcExtractedUserInfo
+            {
+                Nick = nick.ToString(),
+                Ident = identSpan.ToString(),
+                Domain = domainSpan.ToString(),
+                Access = access
+            };
+        }
     }
 
     public static string GetTokenFrom(this string line, int tokenIndex, char delimiter = ' ')
@@ -96,56 +336,11 @@ public static class IrcProtocolHelpers
 
         return span[tokenStartPos..tokenEndPos].ToString();
     }
-
-    public static string ExtractTrailing(this string line, int startIndex) // TODO: make this less weird
+    public static bool IsChannel(this string? parameters)
     {
-        ReadOnlySpan<char> span = line.AsSpan();
-        int currentIndex = 0;
-        int partCount = 0;
+        if (parameters == null || parameters.Length == 0) return false;
 
-        while (partCount < startIndex)
-        {
-            int spaceIndex = span[currentIndex..].IndexOf(' ');
-            if (spaceIndex == -1)
-            {
-                return string.Empty;
-            }
-            currentIndex += spaceIndex + 1;
-            partCount++;
-        }
-
-        ReadOnlySpan<char> remaining = span[currentIndex..];
-        int colonIndex = remaining.IndexOf(':');
-        if (colonIndex == -1)
-        {
-            return remaining.ToString();
-        }
-
-        return remaining[(colonIndex + 1)..].ToString();
-    }
-
-    public static IrcExtractedUserInfo ExtractBasicUserInfo(this string fullNick)
-    {
-        ReadOnlySpan<char> fullNickSpan = fullNick.AsSpan();
-        int nickEnd = fullNickSpan.IndexOf('!');
-        if (nickEnd < 0)
-        {
-            throw new FormatException("Invalid format: '!' not found.");
-        }
-
-        int identEnd = fullNickSpan[(nickEnd + 1)..].IndexOf('@');
-        if (identEnd < 0)
-        {
-            throw new FormatException("Invalid format: '@' not found.");
-        }
-        identEnd += nickEnd + 1;
-
-        return new IrcExtractedUserInfo
-        {
-            Nick = fullNickSpan[..nickEnd].ToString(),
-            Ident = fullNickSpan[(nickEnd + 1)..identEnd].ToString(),
-            Domain = fullNickSpan[(identEnd + 1)..].ToString()
-        };
+        return parameters!.Trim().StartsWith('#');
     }
 
     public static IrcChannelAccessType ModeLetterToAccessType(this char c)
@@ -174,194 +369,5 @@ public static class IrcProtocolHelpers
             '&' => IrcChannelAccessType.SOP,
             _ => IrcChannelAccessType.NONE,
         };
-    }
-
-    /// <summary>
-    /// Extracts channel user mode changes from IRC message parts.
-    /// </summary>
-    /// <param name="messageParts">The IRC message split into parts, where index 3 is the mode string and 4 onward are user names.</param>
-    /// <returns>A dictionary mapping users to their mode changes, with each change as a tuple of (isAdding, accessType).</returns>
-    /// <exception cref="ArgumentException">Thrown if messageParts has fewer than 4 elements.</exception>
-    public static Dictionary<string, List<(bool isAdding, IrcChannelAccessType type)>> ExtractChannelUserModes(this string[] messageParts)
-    {
-        string modeString = messageParts[3];
-        string[] affectedUsers = messageParts[4..];
-        Dictionary<string, List<(bool isAdding, IrcChannelAccessType type)>> userModeChanges = [];
-
-        bool isAdding = false;
-        int userIndex = 0;
-
-        for (int i = 0; i < modeString.Length; i++)
-        {
-            char currentChar = modeString[i];
-            if (currentChar == '+')
-            {
-                isAdding = true;
-            }
-            else if (currentChar == '-')
-            {
-                isAdding = false;
-            }
-            else if (char.IsLetter(currentChar) && userIndex < affectedUsers.Length)
-            {
-                string user = affectedUsers[userIndex];
-                IrcChannelAccessType accessType = currentChar.ModeLetterToAccessType();
-
-                if (accessType != IrcChannelAccessType.NONE)
-                {
-                    if (userModeChanges.TryGetValue(user, out List<(bool isAdding, IrcChannelAccessType type)>? changes))
-                    {
-                        changes.Add((isAdding, accessType));
-                    }
-                    else
-                    {
-                        userModeChanges[user] = [(isAdding, accessType)];
-                    }
-                }
-                userIndex++;
-            }
-        }
-
-        return userModeChanges;
-    }
-
-    public static (string nick, IrcChannelAccessType[] access) ExtractAccessFromNick(this string nick) // TODO: maybe redo this entirely.. this is for 353 and 353 only seems to show 1 channel access mode on IRCnet
-    {
-        Span<IrcChannelAccessType> accessSpan = stackalloc IrcChannelAccessType[5];
-        int accessCount = 0;
-        ReadOnlySpan<char> nickSpan = nick.AsSpan();
-
-        int maxLength = nickSpan.Length < 5 ? nickSpan.Length : 5;
-        for (int i = 0; i < maxLength; i++)
-        {
-            char currentChar = nickSpan[i];
-            IrcChannelAccessType accessType = currentChar.AccessSymbolToAccessType();
-            if (accessType != IrcChannelAccessType.NONE)
-            {
-                accessSpan[accessCount++] = accessType;
-            }
-        }
-
-        string resultNick = accessCount > 0 ? nickSpan[accessCount..].ToString() : nick;
-        IrcChannelAccessType[] resultAccess = accessSpan[..accessCount].ToArray();
-
-        return (resultNick, resultAccess);
-    }
-
-    public static (string nick, IrcChannelAccessType[] access) ExtractAccessFromNick(this ReadOnlySpan<char> nickSpan)
-    {
-        // Use stackalloc for temporary storage of access types
-        Span<IrcChannelAccessType> accessSpan = stackalloc IrcChannelAccessType[5];
-        int accessCount = 0;
-
-        // Iterate through the nick to extract access prefixes
-        int maxLength = nickSpan.Length < 5 ? nickSpan.Length : 5;
-        for (int i = 0; i < maxLength; i++)
-        {
-            char currentChar = nickSpan[i];
-            IrcChannelAccessType accessType = currentChar.AccessSymbolToAccessType();
-            if (accessType != IrcChannelAccessType.NONE)
-            {
-                accessSpan[accessCount++] = accessType;
-            }
-        }
-
-        // Extract the nick without access prefixes
-        string resultNick = accessCount > 0 ? nickSpan[accessCount..].ToString() : nickSpan.ToString();
-        IrcChannelAccessType[] resultAccess = accessSpan[..accessCount].ToArray();
-        return (resultNick, resultAccess);
-    }
-
-    /// <summary>
-    /// This function is to be used for numeric 352
-    /// </summary>
-    ///
-    public static IrcChannelAccessType[] ExtractAccessTypeFrom352(this string modes)
-    {
-        Span<IrcChannelAccessType> accessSpan = stackalloc IrcChannelAccessType[5]; // Max 5 access types
-        int accessCount = 0;
-        ReadOnlySpan<char> modesSpan = modes.AsSpan();
-
-        for (int i = 0; i < modesSpan.Length; i++)
-        {
-            char mode = modesSpan[i];
-            if (char.IsLetter(mode)) continue;
-
-            IrcChannelAccessType accessType = mode.AccessSymbolToAccessType();
-            if (accessType != IrcChannelAccessType.NONE && accessCount < 5)
-                accessSpan[accessCount++] = accessType;
-        }
-
-        return accessSpan[..accessCount].ToArray();
-    }
-
-    public static IrcExtractedUserInfo ExtractUserInfoFrom353(this string fullHostString)
-    {
-        ReadOnlySpan<char> fullHostSpan = fullHostString.AsSpan();
-
-        int nickEnd = fullHostSpan.IndexOf('!');
-        if (nickEnd == -1)
-        {
-            (string nick, IrcChannelAccessType[] access) = ExtractAccessFromNick(fullHostSpan);
-            return new IrcExtractedUserInfo
-            {
-                Nick = nick.ToString(),
-                Access = access
-            };
-        }
-        else
-        {
-            int identEnd = fullHostSpan[(nickEnd + 1)..].IndexOf('@');
-            if (identEnd == -1)
-            {
-                throw new FormatException("Invalid format: '@' not found.");
-            }
-            identEnd += nickEnd + 1;
-
-            ReadOnlySpan<char> nickSpan = fullHostSpan[..nickEnd];
-            ReadOnlySpan<char> identSpan = fullHostSpan[(nickEnd + 1)..identEnd];
-            ReadOnlySpan<char> domainSpan = fullHostSpan[(identEnd + 1)..];
-
-            (string nick, IrcChannelAccessType[] access) = ExtractAccessFromNick(nickSpan);
-
-            return new IrcExtractedUserInfo
-            {
-                Nick = nick.ToString(),
-                Ident = identSpan.ToString(),
-                Domain = domainSpan.ToString(),
-                Access = access
-            };
-        }
-    }
-
-    public static IrcExtractedUserInfo ExtractUserInfoFrom352(this string[] lineParts)
-    {
-        string nick = lineParts[7];
-        string ident = lineParts[4];
-        string domain = lineParts[5];
-        string userAccessModes = lineParts[8];
-        IrcChannelAccessType[] access = userAccessModes.ExtractAccessTypeFrom352();
-        return new IrcExtractedUserInfo
-        {
-            Nick = nick,
-            Ident = ident,
-            Domain = domain,
-            Access = access
-        };
-    }
-
-    public static Dictionary<string, string> ExtractNetworkSettingsFrom005(this string line)
-    {
-        Dictionary<string, string> dictionary = [];
-        Regex regex = new(@"(\S+)=([^\s]+)");
-
-        MatchCollection matches = regex.Matches(line);
-        foreach (Match match in matches)
-        {
-            string key = match.Groups[1].Value;
-            string value = match.Groups[2].Value;
-            dictionary[key] = value;
-        }
-        return dictionary;
     }
 }
